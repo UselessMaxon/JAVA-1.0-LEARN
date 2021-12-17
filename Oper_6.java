9010f2fbc2452c0a6fd8e0c1097d7e0e5792ea10
maxon <zmczksl@gmail.com>
2021-12-17 14:57:56 +0400

    исправление 6

--- Oper_6.java
+++ Oper_6.java
@@ -7,16 +7,27 @@ public class Oper_6 {
 
         System.out.println("Выберите что переводить: 1 - масса, 2 - расстояние");
         int firstQ = in.nextInt();
+
         if (firstQ == 1) {
+            System.out.println("Выберите единицу измерения: 1 - килограмм, 2 - унция, 3 - фунт, 4 - пуд");
+            int SecondQ = in.nextInt();
+            if (SecondQ == 1) {
+                System.out.println("Введите число");
+                float thirdQ = in.nextFloat();
+                System.out.println("Результат:\nКилограммы: "  + thirdQ + "\nУнции: " + (thirdQ * 35.274F));
+                System.out.println("Фунты: " + (thirdQ * 2.2046F) + "\nПуды: " + (thirdQ / 16.38F));
+            }
+        }
+        else if (firstQ == 2) {
             System.out.println("Выберите единицу измерения: 1 - метр, 2 - миля, 3 - ярд, 4 - фут");
             int SecondQ = in.nextInt();
             if (SecondQ == 1) {
                 System.out.println("Введите число");
                 float thirdQ = in.nextFloat();
-                if (thirdQ == 10) {
-                    System.out.println("Результат:\nМетры: 10\nМили: 0.006\nЯрды: 10.94\nФуты: 32.81");
-                }
+                System.out.println("Результат:\nМетры: "  + thirdQ + "\nМили: " + (thirdQ / 1609.34F));
+                System.out.println("Ярды: " + (thirdQ * 1.09361F) + "\nФуты: " + (thirdQ * 3.2808F));
             }
         }
     }
 }
+
