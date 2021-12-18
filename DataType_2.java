9deeaaa497149888acf62e4c04cd762dbe493235
maxon <zmczksl@gmail.com>
2021-12-18 13:05:59 +0400

    исправление 4,5,6

--- DataType_2.java
+++
@@ -1,6 +0,0 @@
-public class DataType_2 {
-    public static void main(String[] args) {
-        double point = 8.5591;
-        byte litteInt = 127;
-    }
-}
--- Oper_5.java
+++ Oper_5.java
@@ -6,10 +6,13 @@ public class Oper_5 {
         Scanner in = new Scanner(System.in);
         float X,Y;
         String operation;
+        System.out.println("Введите число №1");
         X = in.nextInt();
+        System.out.println("Введите число №2");
         Y = in.nextInt();
         in.nextLine(); //очистка сканера
 
+        System.out.println("Введите одну из операций: ‘+’, ‘-’, ‘*’ или ‘/’.");
         operation = in.nextLine();
         switch (operation) {
             case "+": System.out.println(X+Y);
@@ -18,9 +21,15 @@ public class Oper_5 {
                 break;
             case "*": System.out.println(X*Y);
                 break;
-            case "/": System.out.println(X/Y);
+            case "/":
+                if (Y == 0) { // Проверка деления на 0
+                    System.out.println("На 0 делить нельзя!");
+                }
+                else {
+                    System.out.println(X/Y);
+                }
                 break;
-            default:;
+            default:
                 break;
         }
     }
