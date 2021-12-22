aba5b2e4c88d30918e478f6ed16a50f43218ae58
maxon <zmczksl@gmail.com>
2021-12-22 19:41:40 +0400

    исправление 4,5,6

--- Oper_4.java
+++ Oper_4.java
@@ -5,13 +5,17 @@ public class Oper_4 {
 
         Scanner in = new Scanner(System.in);
         float X,Y,Z,average;
+        System.out.println("Введите число №1");
         X = in.nextInt();
+        System.out.println("Введите число №2");
         Y = in.nextInt();
+        System.out.println("Введите число №3");
         Z = in.nextInt();
         average = (X + Y + Z)/3 ;
         System.out.println(average);
 
-        int round = (int)average;
+        float halfAverage = average/2; // деление на 2 среднеарифметического
+        int round = (int)halfAverage; // округление в меньшую сторону
         if (round > 3) {
             System.out.println("Программа выполнена корректно");
         }
--- Oper_6.java
+++ Oper_6.java
@@ -10,24 +10,71 @@ public class Oper_6 {
 
         if (firstQ == 1) {
             System.out.println("Выберите единицу измерения: 1 - килограмм, 2 - унция, 3 - фунт, 4 - пуд");
-            int SecondQ = in.nextInt();
-            if (SecondQ == 1) {
+            int secondQ = in.nextInt();
+            if (secondQ == 1 | secondQ == 2 | secondQ == 3 | secondQ == 4) {
                 System.out.println("Введите число");
                 float thirdQ = in.nextFloat();
-                System.out.println("Результат:\nКилограммы: "  + thirdQ + "\nУнции: " + (thirdQ * 35.274F));
-                System.out.println("Фунты: " + (thirdQ * 2.2046F) + "\nПуды: " + (thirdQ / 16.38F));
+                switch (secondQ) {
+                    case 1:
+                        System.out.println("Результат:\nКилограммы: " + thirdQ + "\nУнции: " + (thirdQ * 35.274F));
+                        System.out.println("Фунты: " + (thirdQ * 2.2046F) + "\nПуды: " + (thirdQ / 16.38F));
+                        break;
+                    case 2:
+                        System.out.println("Результат:\nКилограммы: " + (thirdQ / 35.274F) + "\nУнции: " + thirdQ);
+                        System.out.println("Фунты: " + (thirdQ / 16.0002F) + "\nПуды: " + (thirdQ / 577.788F));
+                        break;
+                    case 3:
+                        System.out.println("Результат:\nКилограммы: " + (thirdQ / 2.2046F) + "\nУнции: " + (thirdQ * 16.0002F));
+                        System.out.println("Фунты: " + thirdQ + "\nПуды: " + (thirdQ / 36.111F));
+                        break;
+                    case 4:
+                        System.out.println("Результат:\nКилограммы: " + (thirdQ * 16.38F) + "\nУнции: " + (thirdQ * 577.788F));
+                        System.out.println("Фунты: " + (thirdQ * 36.111F) + "\nПуды: " + thirdQ);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else {
+                System.out.println("Единица измерения массы не найдена");
             }
         }
+
         else if (firstQ == 2) {
             System.out.println("Выберите единицу измерения: 1 - метр, 2 - миля, 3 - ярд, 4 - фут");
-            int SecondQ = in.nextInt();
-            if (SecondQ == 1) {
+            int secondQ = in.nextInt();
+            if (secondQ == 1 | secondQ == 2 | secondQ == 3 | secondQ == 4) {
                 System.out.println("Введите число");
                 float thirdQ = in.nextFloat();
-                System.out.println("Результат:\nМетры: "  + thirdQ + "\nМили: " + (thirdQ / 1609.34F));
-                System.out.println("Ярды: " + (thirdQ * 1.09361F) + "\nФуты: " + (thirdQ * 3.2808F));
+                switch (secondQ) {
+                    case 1:
+                        System.out.println("Результат:\nМетры: "  + thirdQ + "\nМили: " + (thirdQ / 1609.34F));
+                        System.out.println("Ярды: " + (thirdQ * 1.09361F) + "\nФуты: " + (thirdQ * 3.2808F));
+                        break;
+                    case 2:
+                        System.out.println("Результат:\nМетры: "  + (thirdQ * 1609.34F) + "\nМили: " + thirdQ);
+                        System.out.println("Ярды: " + (thirdQ * 1759.9903F) + "\nФуты: " + (thirdQ * 5279.9226F));
+                        break;
+                   case 3:
+                        System.out.println("Результат:\nМетры: "  + (thirdQ / 1.09361F) + "\nМили: " + (thirdQ / 1759.9903F));
+                        System.out.println("Ярды: " + thirdQ + "\nФуты: " + (thirdQ * 3));
+                        break;
+                     case 4:
+                        System.out.println("Результат:\nМетры: "  + (thirdQ / 3.2808F) + "\nМили: " + (thirdQ / 5279.9226F));
+                        System.out.println("Ярды: " + (thirdQ / 3F) + "\nФуты: " + thirdQ );
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else {
+                System.out.println("Еденица измерения расстояния не найдена");
             }
         }
+
+        else {
+            System.out.println("Физическая величина не найдена");
+        }
+
     }
 }
-
