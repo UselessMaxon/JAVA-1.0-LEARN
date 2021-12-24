86d82a1a0b55e1f1ad4b0f7ee69188335d40a70d
maxon <zmczksl@gmail.com>
2021-12-24 14:43:58 +0400

    добавил 3 и 4

---
+++ Final_4.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Final_4 {
+    public static void main(String[] args) {
+
+        System.out.println("Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает");
+        String answer = "Заархивированный вирус";
+        Scanner in = new Scanner(System.in);
+
+        boolean checkHelp = false;
+
+        for (int i = 1; i <= 3; i++) {
+            String input = in.nextLine();
+
+            if (input.equalsIgnoreCase("Подсказка")) {
+                if (i==1){
+                    System.out.println("trojan.zip");
+                    checkHelp = true;
+                    input = in.nextLine();
+                }
+                else {
+                    System.out.println("Подсказка уже недоступна");
+                    input = in.nextLine();
+                }
+            }
+
+            if (input.equalsIgnoreCase(answer)) {
+                System.out.println("Правильно!");
+                return;
+            }
+            else {
+                if (checkHelp) {
+                    System.out.println("Обидно, приходи в другой раз");
+                    return;
+                }
+                else {
+                    System.out.println("Подумай еще!");
+                }
+            }
+        }
+
+        System.out.println("Обидно, приходи в другой раз");
+    }
+}
